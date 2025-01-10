bc1a965d1ef3e461b4d1276bed7b02e1f46764e5
NickshanJ <nickshan001@gmail.com>
2025-01-10 13:21:26 +0530

    Update

--- src/CartIcon.jsx
+++ src/CartIcon.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FaShoppingCart } from 'react-icons/fa';
 
 const CartIcon = ({ itemCount, onClick }) => {
   return (
     <div className="relative cursor-pointer" onClick={onClick}>
-      <FontAwesomeIcon icon={faShoppingCart} size="2x" />
+      <FaShoppingCart size={32} />
       {itemCount > 0 && (
         <span className="absolute top-0 -right-2 inline-flex items-center justify-center h-5 w-5 text-xs font-bold leading-none text-white bg-teal-600 rounded-full">
           {itemCount}
